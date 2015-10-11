8195526b6a57b7d37142592b2de1ab4b7e4e382c
Emiliano Andre <emilianoandre@gmail.com>
2015-10-11 00:49:12 -0300

    Added detail view with Wiki links

--- GotProject/app/app.js
+++ GotProject/app/app.js
@@ -1,45 +1,50 @@
-angular.module("myApp", ["ngRoute"])
-        .value("predictions", [{
-        	id: 0,
-        	name: 'Jon Snow',
-          	family: 'Stark',
-          	season: 1,
-          	episode: 3
-        }, {
-        	id: 1,
-          	name: 'Sansa Stark',
-          	family: 'Stark',
-          	season: 1,
-          	episode: 3
-        }, {
-        	id: 2,
-          	name: 'Tyrion Lannister',
-          	family: 'Lannister',
-          	season: 1,
-          	episode: 3
-        }])
-        .constant("VIEWS", {
-        	listView: "app/view/listView.html",
-        	formView: "app/view/formView.html",
-        	homeView: "app/view/homeView.html"
-        }).value("filtered", 
-        		false)
-        .config(["$routeProvider", "VIEWS", function($routeProvider, VIEWS) {
-          $routeProvider.when("/", {
-            controller: "ListController",
-            templateUrl: VIEWS.listView
-          }).when("/detail/:id", {
-            controller: "DetailController",
-            templateUrl: VIEWS.formView
-          }).when("/delete/:id", {
-              controller: "DeleteController",
-              templateUrl: VIEWS.listView
-          }).when("/add", {
-              controller: "AddController",
-              templateUrl: VIEWS.formView
-          }).when("/home", {              
-              templateUrl: VIEWS.homeView
-            }).otherwise({
-            redirectTo: "/"
-          });	
-        }])
+//Creating the app
+var app = angular.module("gotApp", ["ngRoute"])
+
+//Adding the global variables, view constants and routing logic
+app.value("predictions", [{
+	id: 0,
+	name: 'Jon Snow',
+	family: 'Stark',
+	season: 1,
+	episode: 3
+}, {
+	id: 1,
+	name: 'Sansa Stark',
+	family: 'Stark',
+	season: 2,
+	episode: 1
+}, {
+	id: 2,
+	name: 'Tyrion Lannister',
+	family: 'Lannister',
+	season: 4,
+	episode: 5
+}])
+.constant("VIEWS", {
+	listView: "app/view/listView.html",
+	formView: "app/view/formView.html",
+	homeView: "app/view/homeView.html",
+	detailView: "app/view/detailView.html"
+}).config(["$routeProvider", "VIEWS", function($routeProvider, VIEWS) {
+	$routeProvider.when("/", {
+		controller: "ListController",
+		templateUrl: VIEWS.listView
+	}).when("/edit/:id", {
+		controller: "EditController",
+		templateUrl: VIEWS.formView
+	}).when("/delete/:id", {
+		controller: "DeleteController",
+		templateUrl: VIEWS.listView
+	}).when("/add", {
+		controller: "AddController",
+		templateUrl: VIEWS.formView
+	}).when("/home", {              
+		templateUrl: VIEWS.homeView
+	}).when("/detail/:id", {
+		controller: "DetailController",
+		templateUrl: VIEWS.detailView
+	}).otherwise({
+		redirectTo: "/"
+	});	
+}])
